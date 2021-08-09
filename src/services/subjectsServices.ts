3cdc25ce19ea29210d5dd376cfbbd2171dbbc34b
thiagoribb <thiribeiro142@gmail.com>
2021-08-09 16:46:38 -0300

    Preparando revisão de código

--- src/services/subjectsServices.ts
+++
@@ -1,8 +0,0 @@
-import { getRepository } from "typeorm";
-import Subject from "../entities/subjects";
-
-async function getAllSubjects(): Promise<Array<Subject>> {
-    return await getRepository(Subject).find()
-}
-
-export { getAllSubjects }
--- tests/factories/subjectFactory.ts
+++
@@ -1,9 +0,0 @@
-import { getRepository, ObjectLiteral } from "typeorm";
-import Subject from "../../src/entities/subjects";
-
-async function getSubjectParam():Promise<string> {
-    const subjects = await getRepository(Subject).find()
-    return(subjects[Math.floor(Math.random()*subjects.length)].name)
-}
-
-export { getSubjectParam }
